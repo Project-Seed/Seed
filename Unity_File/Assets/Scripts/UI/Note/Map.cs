16aae8c4f2531c9d903c601aef8b648beb70a3bd
ruen_mainpc <ruen346@gmail.com>
2020-01-16 00:11:03 +0900

    시간, 위치(가상) 적용

--- Unity_File/Assets/Scripts/UI/Note/Map.cs
+++ Unity_File/Assets/Scripts/UI/Note/Map.cs
@@ -7,6 +7,9 @@ public class Map : MonoBehaviour
 {
     public Text TimePosition;
 
+    string time_switch = null;
+    int time_hour;
+    int time_minute;
 
     void Start()
     {
@@ -15,6 +18,21 @@ public class Map : MonoBehaviour
 
     void Update()
     {
+        Vector3 pos = Input.mousePosition;
 
+        if(GameSystem.instance.time < 60 * 12)
+        {
+            time_switch = "AM";
+            time_hour = (int)GameSystem.instance.time / 60;
+            time_minute = (int)GameSystem.instance.time % 60;
+        }
+        else
+        {
+            time_switch = "PM";
+            time_hour = ((int)GameSystem.instance.time - 60 * 12) / 60;
+            time_minute = ((int)GameSystem.instance.time - 60 * 12) % 60;
+        }
+
+        TimePosition.text = "시간 " + time_switch + " " + time_hour.ToString() + ":" + time_minute.ToString() + "\n위치 (" + ((int)pos.x).ToString() + ", " + ((int)pos.y).ToString() + ")";
     }
 }
