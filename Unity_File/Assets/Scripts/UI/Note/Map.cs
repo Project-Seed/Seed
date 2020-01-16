bfb978085295a9425bfa64e683eba1ab950863bc
ruen_mainpc <ruen346@gmail.com>
2020-01-16 23:02:31 +0900

    마크 구현 완료

--- Unity_File/Assets/Scripts/UI/Note/Map.cs
+++ Unity_File/Assets/Scripts/UI/Note/Map.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Map : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class Map : MonoBehaviour
     public GameObject map_bg;
     Vector3 move_start; // 이동 할때 누르기 시작시 좌표
     Vector3 move_now; // 이동 할때 누르기 시작할때 이미지 좌표
+
+    bool push_bg = false;
+    bool push_mark = false;
+    public GameObject mark_maker_ob;
+    public GameObject mark;
+    GameObject marks_ob; // 눌러진 마크
+    public Text mark_make_text;
+    public Text mark_dis_text;
+    int mark_click_mod = 0; // 지도클릭 0, 마크클릭 1
 
     void Update()
     {
@@ -58,5 +68,61 @@ public class Map : MonoBehaviour
         {
             map_bg.transform.localScale = new Vector3(map_bg.transform.localScale.x - 0.1f, map_bg.transform.localScale.y - 0.1f, 1);
         }
-     }
-}
+
+
+
+        // 마커 생성
+        if (Input.GetMouseButtonDown(1) && push_bg == true && push_mark == false && mark_maker_ob.activeSelf == false)
+        {
+            mark_maker_ob.SetActive(true);
+            mark_maker_ob.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            mark_make_text.color = Color.white;
+            mark_dis_text.color = Color.grey;
+            mark_click_mod = 0;
+        }
+        else if (Input.GetMouseButtonDown(1) && push_bg == true && push_mark == true && mark_maker_ob.activeSelf == false)
+        {
+            mark_maker_ob.SetActive(true);
+            mark_maker_ob.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            mark_make_text.color = Color.grey;
+            mark_dis_text.color = Color.white;
+            mark_click_mod = 1;
+        }
+    }
+
+    // 마커 생성
+    public void on_bg() // 지도 위에 커서가 있을경우
+    {
+        push_bg = true;
+    }
+    public void off_bg() // 지도 위에 커서가 없을경우
+    {
+        push_bg = false;
+    }
+    public void on_mark(GameObject mark_ob) // 마커 위에 커서가 있을경우
+    {
+        push_mark = true;
+        marks_ob = mark_ob;
+    }
+    public void off_mark(GameObject mark_ob) // 마커 위에 커서가 없을경우
+    {
+        push_mark = false;
+    }
+
+    public void mark_make()
+    {
+        if (mark_click_mod == 0)
+        {
+            GameObject marks = Instantiate(mark, new Vector3(mark_maker_ob.transform.position.x, mark_maker_ob.transform.position.y, 0), Quaternion.identity, map_bg.transform); // ItemSpawner 밑 자식으로 복제
+            mark_maker_ob.SetActive(false);
+        }
+    }
+    public void mark_dis()
+    {
+        if (mark_click_mod == 1)
+        {
+            mark_maker_ob.SetActive(false);
+            Destroy(marks_ob);
+        }
+    }
+} 
---
+++ Unity_File/Assets/Scripts/UI/Note/Mark.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mark : MonoBehaviour
+{
+    GameObject map;
+
+    private void Awake()
+    {
+        map = GameObject.Find("Map");
+    }
+
+    public void on_mark() // 마커 위에 커서가 있을경우
+    {
+        map.GetComponent<Map>().on_mark(gameObject);
+    }
+    public void off_mark() // 마커 위에 커서가 없을경우
+    {
+        map.GetComponent<Map>().off_mark(gameObject);
+    }
+}
