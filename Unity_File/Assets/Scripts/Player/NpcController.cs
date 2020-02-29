f0a5159f9137026b344645d7b91e0ba793445c81
ruen_mainpc <ruen346@gmail.com>
2020-02-29 16:26:42 +0900

    퀘스트 관련 스크립트 정리

--- Unity_File/Assets/Scripts/Player/NpcController.cs
+++ Unity_File/Assets/Scripts/Player/NpcController.cs
@@ -7,15 +7,7 @@ public class NpcController : MonoBehaviour
 {
     public GameObject dialogue; // 다이얼로그
 
-    public Text text;
-    public Text name;
-    public GameObject texts;
-    public GameObject names;
-    public Camera cameras;
-
     public GameObject name_position;
-
-    bool check = false;
 
     void Start()
     {
@@ -24,40 +16,21 @@ public class NpcController : MonoBehaviour
 
     void Update()
     {
-        if (check == true)
-        {
-            Vector3 screenPos = cameras.WorldToScreenPoint(transform.position);
-            Vector3 screenPos2 = cameras.WorldToScreenPoint(name_position.transform.position);
-            texts.transform.position = new Vector3(screenPos.x, screenPos.y, text.transform.position.z);
-            names.transform.position = new Vector3(screenPos2.x, screenPos2.y, name.transform.position.z);
-            name.text = "코난";
 
-            if (Input.GetKeyDown(KeyCode.H))
-            {
-                if (dialogue.activeSelf == false)
-                {
-                    dialogue.SetActive(true);
-                }
-            }
-        }
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.name == "Player")
         {
-            check = true;
-            texts.SetActive(true);
-            names.SetActive(true);
+            dialogue.GetComponent<Dialogue>().quest_on(gameObject, name_position, "코난");
         }
     }
     private void OnTriggerExit(Collider collision)
     {
         if (collision.name == "Player")
         {
-            check = false;
-            texts.SetActive(false);
-            names.SetActive(false);
+            dialogue.GetComponent<Dialogue>().quest_off();
         }
     }
 }
--- Unity_File/Assets/Scripts/UI/Dialogue.cs
+++ Unity_File/Assets/Scripts/UI/Dialogue.cs
@@ -1,18 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject npc_name;
+    public GameObject talk_guide;
+    public GameObject dialogue_box;
+
+    public Text name_text;
+    public GameObject quest_state;
+    public Image quest_image;
+
+    public Sprite quest_start;
+    public Sprite quest_ing;
+    public Sprite quest_end;
+
+    bool quest_bool = false;
+    bool talk_bool = false;
+
+
+    public Camera cameras;
+    GameObject npc_ob;
+    GameObject name_position;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (quest_bool == true)
+        {
+            Vector3 guide_pos = cameras.WorldToScreenPoint(npc_ob.transform.position);
+            Vector3 name_pos = cameras.WorldToScreenPoint(name_position.transform.position);
+
+            npc_name.transform.position = new Vector3(name_pos.x, name_pos.y, npc_name.transform.position.z);
+            talk_guide.transform.position = new Vector3(guide_pos.x, guide_pos.y, talk_guide.transform.position.z);
+
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                if (dialogue_box.activeSelf == false)
+                {
+                    dialogue_box.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void quest_on(GameObject npc_obs, GameObject name_positions, string name)
+    {
+        npc_name.SetActive(true);
+        talk_guide.SetActive(true);
+
+        npc_ob = npc_obs;
+        name_position = name_positions;
+        name_text.text = name;
+
+        quest_bool = true;
+    }
+
+    public void quest_off()
+    {
+        npc_name.SetActive(false);
+        talk_guide.SetActive(false);
+
+        quest_bool = false;
+    }
+
+    public void talk_box()
+    {
+
     }
 }
