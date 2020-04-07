9e18a5c4dfaf6e826c94e94a2d500ae44d4d97c6
ruen_mainpc <ruen346@gmail.com>
2020-04-07 23:16:13 +0900

    아이템 앞에 가면 뜨게 기반

---
+++ Unity_File/Assets/Scripts/Player/ItemCollier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollier : MonoBehaviour
+{
+    public GameObject item_ui;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Item_text")
+        {
+            item_ui.SetActive(true);
+            item_ui.GetComponent<Item_text>().text_reset(collision.name);
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Item_text")
+        {
+            item_ui.SetActive(false);
+        }
+    }
+}
---
+++ Unity_File/Assets/Scripts/UI/Item_text.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item_text : MonoBehaviour
+{
+    public Text text;
+
+    public void text_reset(string name)
+    {
+        switch(name)
+        {
+            case "Box":
+                text.text = "[F] : 박스";
+                break;
+        }
+    }
+}
