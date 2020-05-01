7bdbbf322b15c9f23e997af11ee214c953312c2b
mminionss <33679011+mminionss@users.noreply.github.com>
2020-05-02 00:03:22 +0900

    오브젝트 복사 툴 만드는중. ..!
    풀이랑 나무 말고 일단 건물만 배치하고 있어주세욥 일요일에 .. 돌아와서 하겠ㅅ읍니다

---
+++ Unity_File/Assets/Editor/DisplayWindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+class DisplayWindow : EditorWindow
+{
+    [MenuItem("Window/Display")]
+    static void Init()
+    {
+        DisplayWindow window = (DisplayWindow)GetWindow(typeof(DisplayWindow));
+        window.Show();
+    }
+
+    Transform mousePos;
+            GameObject obj;
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        {
+            //배치할 오브젝트 선택
+            obj = (GameObject)EditorGUILayout.ObjectField("Select Object", obj, typeof(GameObject), true);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                mousePos.position = Input.mousePosition;
+                if (obj != null)
+                    Instantiate(obj, mousePos);
+                else
+                    System.Console.WriteLine("OBJ NULL");
+            }
+
+            if (GUILayout.Button("Clear"))
+            {
+                obj = null;
+            }
+        }
+        GUILayout.EndHorizontal();
+
+    }
+}
+
