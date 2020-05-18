d3e9386c6db3a32e940515cf87dc0561e2595555
ruen_mainpc <ruen346@gmail.com>
2020-05-19 00:06:22 +0900

    키 가이드 하는중

---
+++ Unity_File/Assets/Scripts/UI/Key_guide.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_guide : MonoBehaviour
+{
+    public static Key_guide instance; // 현재 클레스를 인스턴트화
+
+    public GameObject talk;
+    public GameObject item;
+
+    public static Key_guide Instance
+    {
+        get { return instance; }
+    }
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void talk_on()
+    {
+        talk.SetActive(true);
+    }
+    public void talk_off()
+    {
+        talk.SetActive(false);
+    }
+}
