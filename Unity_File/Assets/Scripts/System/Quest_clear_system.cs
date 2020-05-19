10a133d11916f80e558079565dbc9d249e435c2f
mminionss <33679011+mminionss@users.noreply.github.com>
2020-05-20 04:20:50 +0900

    퀘스트 시스템 정리
    엑셀 스크립트, 코드 수정

--- Unity_File/Assets/Scripts/System/Quest_clear_system.cs
+++ Unity_File/Assets/Scripts/System/Quest_clear_system.cs
@@ -26,6 +26,14 @@ public class Quest_clear_system : MonoBehaviour
 
         if (GameSystem.instance.quest_state[3] == 1)
             GameSystem.instance.quest_state[3] = 2;
+
+        if (GameSystem.instance.quest_state[4] == 1 &&
+            GameSystem.instance.item_num["coal"] >= 5)
+            GameSystem.instance.quest_state[4] = 2;
+
+        if (GameSystem.instance.quest_state[5] == 1 &&
+            GameSystem.instance.item_num["coal"] >= 1)
+            GameSystem.instance.quest_state[5] = 2;
     }
 
     public string num_data(int num)
@@ -39,6 +47,13 @@ public class Quest_clear_system : MonoBehaviour
             case 3:
                 data = "잠시 후 다시 말을 걸어보자";
                 break;
+            case 4:
+                data = "석탄" + GameSystem.instance.item_num["coal"].ToString() + " / 5";
+                break;
+
+            case 5:
+                data = "끈적이는 이끼" + GameSystem.instance.item_num["sticky_moss"].ToString() + " / 1";
+                break;
         }
 
         return data;
@@ -49,6 +64,10 @@ public class Quest_clear_system : MonoBehaviour
         switch (num)
         {
             case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
                 Eat_system.instance.eat_item("aliquot_part");
                 break;
         }
