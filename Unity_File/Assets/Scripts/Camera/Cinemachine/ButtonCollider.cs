0eea152729f30e4185082da6c8debe1f2f522724
mminionss <33679011+mminionss@users.noreply.github.com>
2020-06-18 16:58:23 +0900

    스크립트 백업

---
+++ Unity_File/Assets/Scripts/Camera/Cinemachine/ButtonCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonCollider : MonoBehaviour
+{
+    public UnityEvent onEnter;
+    public UnityEvent onSpacebar;
+    public UnityEvent onReturn;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (onEnter != null)
+            onEnter.Invoke();
+    }
+
+    public void OnTriggerStay()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onSpacebar != null)
+            {
+                onSpacebar.Invoke();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (onReturn != null)
+            {
+                onReturn.Invoke();
+            }
+        }
+    }
+}
---
+++ Unity_File/Assets/Scripts/Camera/Cinemachine/TimelineController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    public void Play()
+    {
+        playableDirector.Play();
+    }
+}
