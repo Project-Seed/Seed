79207327e64716addca9c437339650cf846d676f
ruen_mainpc <ruen346@gmail.com>
2020-07-06 22:50:48 +0900

    수정

--- Unity_File/Assets/Scripts/UI/Note/Map.cs
+++ Unity_File/Assets/Scripts/UI/Note/Map.cs
@@ -37,6 +37,7 @@ public class Map : MonoBehaviour
         // 시간 위치 표시
         Vector3 pos = Input.mousePosition;
 
+        /*
         if(GameSystem.instance.time < 60 * 12)
         {
             time_switch = "AM";
@@ -49,6 +50,7 @@ public class Map : MonoBehaviour
             time_hour = ((int)GameSystem.instance.time - 60 * 12) / 60;
             time_minute = ((int)GameSystem.instance.time - 60 * 12) % 60;
         }
+        */
 
         TimePosition.text = "시간 " + time_switch + " " + time_hour.ToString() + ":" + time_minute.ToString() + "\n위치 (" + ((int)pos.x).ToString() + ", " + ((int)pos.y).ToString() + ")";
 
