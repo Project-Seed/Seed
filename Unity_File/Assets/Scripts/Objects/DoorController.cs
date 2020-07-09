319d2e78354402773c89507970dfba631cfd4829
mminionss <33679011+mminionss@users.noreply.github.com>
2020-07-09 18:17:25 +0900

    DoorController를 살려라

---
+++ Unity_File/Assets/Scripts/Objects/DoorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // 내가 보이니 //
+    public Animator open;
+
+    public void OpenDoor()
+    {
+        if (!open.GetBool("isOpen"))
+        {
+            open.SetBool("isOpen", true);
+        }
+    }
+    public void CloseDoor()
+    {
+        if (open.GetBool("isOpen"))
+        {
+            open.SetBool("isOpen", false);
+        }
+    }
+
+    public void KeyAppear()
+    {
+        Key_guide.instance.door_on();
+    }
+
+    public void KeyDisAppear()
+    {
+        Key_guide.instance.door_off();
+    }
+
+}
