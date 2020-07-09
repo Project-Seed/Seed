fbdfd5949c811444174373c53ea55db36821c083
ruen <34498109+ruen346@users.noreply.github.com>
2020-07-09 12:15:21 +0900

    손이 방해하지 않음

---
+++ Unity_File/Assets/Scripts/Seed/blue_seed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blue_seed : MonoBehaviour
+{
+    public PlayerController pc;
+    public GameObject blue;
+
+    private void Awake()
+    {
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            pc.hang_ob = blue;
+            pc.hang_crash = true;
+
+            Key_guide.instance.climb_on();
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            pc.hang_crash = false;
+
+            Key_guide.instance.climb_off();
+        }
+    }
+}
