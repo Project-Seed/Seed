8cdf83b9dbe5f6d20b38ff3a4ffb71377b9bf458
ruen <34498109+ruen346@users.noreply.github.com>
2020-07-14 20:08:24 +0900

    로딩함

--- Unity_File/Assets/Scripts/System/LoadingSceneManager.cs
+++ Unity_File/Assets/Scripts/System/LoadingSceneManager.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadingSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static string nextScene;
+
+    public Slider slider;
+
+    private void Start()
     {
-        
+        StartCoroutine(LoadScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    string nextSceneName;
+    public static void LoadScene(string sceneName)
     {
-        
+        nextScene = sceneName;
+        SceneManager.LoadScene("Loading_Scene");
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0.0f;
+        while (!op.isDone)
+        {
+            Debug.Log(timer);
+            yield return null;
+
+            timer += Time.deltaTime;
+
+            if (op.progress >= 0.9f)
+            {
+                slider.value = timer;
+
+                if (slider.value == 1.0f)
+                    op.allowSceneActivation = true;
+            }
+            else
+            {
+                slider.value = timer;
+                if (slider.value >= op.progress)
+                {
+                    timer = 0f;
+                }
+            }
+        }
     }
 }
