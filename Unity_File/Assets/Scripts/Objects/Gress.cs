03d049ad9798b3a637bf2d60d5e7e8727e3563e6
ruen_mainpc <ruen346@gmail.com>
2020-07-17 23:31:43 +0900

    잠자는 풀을 해 보았따

---
+++ Unity_File/Assets/Scripts/Objects/Gress.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gress : MonoBehaviour
+{
+    public List<GameObject> gress;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gress[0].transform.eulerAngles.x);
+
+        if (other.gameObject.name == "Player")
+        {
+            for (int i = 0; i < gress.Count; i++)
+            {
+                gress[i].transform.rotation = Quaternion.Euler(new Vector3(-30, gress[i].transform.eulerAngles.y, 0));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            for (int i = 0; i < gress.Count; i++)
+            {
+                gress[i].transform.rotation = Quaternion.Euler(new Vector3(0, gress[i].transform.eulerAngles.y, 0));
+            }
+        }
+    }
+}
