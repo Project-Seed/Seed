a752a8e43a903d6f8e67fe04ac795677ca330100
ruen_mainpc <ruen346@gmail.com>
2020-07-26 23:48:29 +0900

    돌 굴러가유

---
+++ Unity_File/Assets/Scripts/Obstacle/Rolling_rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling_rock : MonoBehaviour
+{
+    public int cooltime = 0;
+    private void Awake()
+    {
+        Destroy(gameObject, 30);
+    }
+
+    IEnumerator die()
+    {
+        cooltime = 1;
+        GameObject.Find("State").GetComponent<State>().hp_down(4);
+        yield return new WaitForSeconds(3f);
+
+        Destroy(gameObject);
+
+        for (int i=0; i<50; i++)
+        {
+
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player" && cooltime == 0)
+        {
+            StartCoroutine(die());
+        }
+    }
+}
