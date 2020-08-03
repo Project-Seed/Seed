24ee272b4571fe8656d01e118bc4570e8e1681ac
ruen_mainpc <ruen346@gmail.com>
2020-08-03 21:02:09 +0900

    발사할때 해당 모델이 나가게
    어째서 지금까지 회색 이상한 씨앗이 나간건데 아무도 이상해 하지 않았을까나

--- Unity_File/Assets/Scripts/Seed/Plant.cs
+++ Unity_File/Assets/Scripts/Seed/Plant.cs
@@ -13,10 +13,58 @@ class Plant : MonoBehaviour
     public string seed_name;
     public Vector3 red_go; // 빨간 씨앗이 자라는 방향
 
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
+    public List<Mesh> meshes;
+    public List<Material> materials;
+
     private void Start()
     {
         plant = Resources.Load(seed_name, typeof(GameObject)) as GameObject;
 
+        switch(seed_name)
+        {
+            case "blue_seed":
+                meshFilter.mesh = meshes[0];
+                meshRenderer.material = materials[0];
+                break;
+
+            case "brown_seed":
+                meshFilter.mesh = meshes[1];
+                meshRenderer.material = materials[1];
+                break;
+
+            case "green_seed":
+                meshFilter.mesh = meshes[2];
+                meshRenderer.material = materials[2];
+                break;
+
+            case "lime_seed":
+                meshFilter.mesh = meshes[3];
+                meshRenderer.material = materials[3];
+                break;
+
+            case "purple_seed":
+                meshFilter.mesh = meshes[4];
+                meshRenderer.material = materials[4];
+                break;
+
+            case "red_seed":
+                meshFilter.mesh = meshes[5];
+                meshRenderer.material = materials[5];
+                break;
+
+            case "white_seed":
+                meshFilter.mesh = meshes[6];
+                meshRenderer.material = materials[6];
+                break;
+
+            case "yellow_seed":
+                meshFilter.mesh = meshes[7];
+                meshRenderer.material = materials[7];
+                break;
+        }
     }
 
     public void PlantSeed(Vector3 pos, Vector3 normal, bool red, bool blue)
