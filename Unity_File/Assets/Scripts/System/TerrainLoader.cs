996ae07ad613ac585d56036c353ee04bd742f475
mminionss <33679011+mminionss@users.noreply.github.com>
2020-08-08 19:32:27 +0900

    터레인 로드

--- Unity_File/Assets/Scripts/System/TerrainLoader.cs
+++ Unity_File/Assets/Scripts/System/TerrainLoader.cs
@@ -2,23 +2,28 @@
 
 class TerrainLoader : MonoBehaviour
 {
-
     public GameObject[] terrain;
-    private MeshRenderer renderer;
+   // private MeshRenderer renderer;
 
-    private void Start()
-    {
-        renderer = GetComponent<MeshRenderer>();
-        renderer.enabled = false;
-    }
+    //private void Start()
+    //{
+    //    renderer = GetComponent<MeshRenderer>();
+    //    renderer.enabled = false;
+    //}
 
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag.Equals("Player"))
+    //    {
+    //        for (int i = 0; i < terrain.Length; ++i)
+    //            terrain[i].SetActive(true);
+    //    }
+    //}
+
+    public void Load()
     {
-        if (other.tag.Equals("Player"))
-        {
-            for (int i = 0; i < terrain.Length; ++i)
-                terrain[i].SetActive(true);
-        }
+        for (int i = 0; i < terrain.Length; ++i)
+            terrain[i].SetActive(true);
     }
 }
 
---
+++ Unity_File/Assets/Scripts/System/TerrainManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TerrainManager : MonoBehaviour
+{
+    public GameObject[] terrain;
+    public UnityEvent onCall; //호출당했음
+
+    public void CallTerrain()
+    {
+        if (onCall != null)
+            onCall.Invoke();
+    }
+}
