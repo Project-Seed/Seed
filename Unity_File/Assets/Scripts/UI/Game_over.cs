62f126e6e85f402e1825879686c91168e31214f9
ruen <34498109+ruen346@users.noreply.github.com>
2020-08-10 03:47:26 +0900

    수정232323

--- Unity_File/Assets/Scripts/UI/Game_over.cs
+++ Unity_File/Assets/Scripts/UI/Game_over.cs
@@ -19,6 +19,8 @@ public class Game_over : MonoBehaviour
         {
             texts[i].color = new Color(1, 1, 1, 0);
         }
+
+        GameSystem.instance.sound_start(2);
     }
 
     public void image_on()
